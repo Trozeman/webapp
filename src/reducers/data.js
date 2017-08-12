175cfc9c2e884585e550eef20f9096dc570206ff
Peter BLITZ <peter@blitz.org.ua>
2017-08-12 15:51:17 +0300

    fix reducer (dublicate data)

--- src/reducers/data.js
+++ src/reducers/data.js
@@ -3,7 +3,7 @@ const initialState = [];
 const data = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_DATA_SUCCESS':
-      return [ ...state, ...action.payload ]
+      return [ ...action.payload ]
 
     default:
       return state
