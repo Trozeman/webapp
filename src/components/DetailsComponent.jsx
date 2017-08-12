b9e74b7b781805cb755d54cb9ff103c6bb348f66
Peter BLITZ <peter@blitz.org.ua>
2017-08-12 17:51:21 +0300

    goBack

--- src/components/DetailsComponent.jsx
+++ src/components/DetailsComponent.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 class DetailsComponent extends React.Component {
   render() {
-    const { pointDetails } = this.props;
+    const { pointDetails, goBack } = this.props;
     const { name, description } = pointDetails;
     return(
       <div className="details-components">
-        <div className="TopLine">
-         <svg xmlns="http://www.w3.org/2000/svg" viewBox="2231 776 16 16">
-          <path id="path" d="M20,11H7.8l5.6-5.6L12,4,4,12l8,8,1.4-1.4L7.8,13H20V11Z" transform="translate(2227 772)"/>
-        </svg>
+        <div className="TopLine" onClick={()=>goBack()}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="2231 776 16 16">
+            <path id="path" d="M20,11H7.8l5.6-5.6L12,4,4,12l8,8,1.4-1.4L7.8,13H20V11Z" transform="translate(2227 772)"/>
+          </svg>
           <p>Список послуг</p>
         </div>
-          <img src="http://placehold.it/600x450/" /> 
+          <img src="http://placehold.it/600x450/" />
           <div className="MainName">{name}</div>
           <div className="MainDesc">{description}</div>
       </div>
