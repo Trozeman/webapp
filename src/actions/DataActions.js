1ac165397f7eb3be596a8bf740fb6ff0d242301a
Peter BLITZ <peter@blitz.org.ua>
2017-08-12 14:24:15 +0300

    sample data

--- src/actions/DataActions.js
+++ src/actions/DataActions.js
@@ -1,31 +1,9 @@
 import axios from 'axios';
 
+import sampleData from '../data/sample.js';
+
 const API_PATH = '/api/v1';
 
-const sampleData = [
-  {
-    id: Math.random(),
-    name: 'Point Name',
-    coordinates: {
-      latitude: '300000000',
-      longitude: '400000000',
-    },
-    description: 'Blabla wejghw kjeg kerjg kgjr k regk jkjerhg kjh er',
-    address: 'Степана Бандери 12/56',
-    contacts: 'tel: 8 800 800 80 80'
-  },
-  {
-    id: Math.random(),
-    name: 'Point Name1',
-    coordinates: {
-      latitude: '300000001',
-      longitude: '400000001',
-    },
-    description: '1Blabla wejghw kjeg kerjg kgjr k regk jkjerhg kjh er',
-    address: 'Степана Бандери 22/56',
-    contacts: 'tel: 8 800 800 80 80'
-  },
-];
 
 export function fetchDataAction(data) {
   return (dispatch) => {
---
+++ src/data/sample.js
@@ -0,0 +1,24 @@
+export default [
+  {
+    id: Math.random(),
+    name: 'Point Name',
+    coordinates: {
+      latitude: '300000000',
+      longitude: '400000000',
+    },
+    description: 'Blabla wejghw kjeg kerjg kgjr k regk jkjerhg kjh er',
+    address: 'Степана Бандери 12/56',
+    contacts: 'tel: 8 800 800 80 80'
+  },
+  {
+    id: Math.random(),
+    name: 'Point Name1',
+    coordinates: {
+      latitude: '300000001',
+      longitude: '400000001',
+    },
+    description: '1Blabla wejghw kjeg kerjg kgjr k regk jkjerhg kjh er',
+    address: 'Степана Бандери 22/56',
+    contacts: 'tel: 8 800 800 80 80'
+  },
+]
