20819b92ce12a36887b9851e57d9e64090b24c74
Trozeman <trozeman@gmail.com>
2017-08-12 17:32:42 +0300

    UI 3 path

--- src/components/DetailsComponent.jsx
+++ src/components/DetailsComponent.jsx
@@ -5,10 +5,16 @@ class DetailsComponent extends React.Component {
     const { pointDetails } = this.props;
     const { name, description } = pointDetails;
     return(
-      <div>
-        DetailsComponent
-        <div>{name}</div>
-        <div>{description}</div>
+      <div className="details-components">
+        <div className="TopLine">
+         <svg xmlns="http://www.w3.org/2000/svg" viewBox="2231 776 16 16">
+          <path id="path" d="M20,11H7.8l5.6-5.6L12,4,4,12l8,8,1.4-1.4L7.8,13H20V11Z" transform="translate(2227 772)"/>
+        </svg>
+          <p>Список послуг</p>
+        </div>
+          <img src="http://placehold.it/600x450/" /> 
+          <div className="MainName">{name}</div>
+          <div className="MainDesc">{description}</div>
       </div>
     )
   }
