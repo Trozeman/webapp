41d1f1369ce29b1e10504bc69126b5f4a793a3bf
Peter BLITZ <peter@blitz.org.ua>
2017-08-13 17:09:00 +0300

    icon

--- src/components/ListComponent.jsx
+++ src/components/ListComponent.jsx
@@ -7,11 +7,11 @@ import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'm
 import ArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
 
 function PlaceComponent({ place, goTo }) {
-  const { name, distance, price, _id, description } = place
+  const { name, distance, price, _id, description, image } = place
   return (
     <Card style={{marginBottom: '20px'}} onTouchTap={()=>goTo(`/details/${_id}`)}>
       <CardMedia>
-        <img style={{borderRadius:'5px 5px 0px 0px', filter: 'brightness(50%)'}} src="http://placehold.it/600x250/" alt="" />
+        <img style={{borderRadius:'5px 5px 0px 0px', filter: 'brightness(50%)'}} src={image} alt="" />
       </CardMedia>
       <CardTitle title={name} subtitle={<div>{`${price} гривень`} {`${distance} м`}</div>} />
     </Card>
