b2c8342fb102da263b17d61cfea83b04dca05f8b
Peter BLITZ <peter@blitz.org.ua>
2017-08-13 16:44:07 +0300

    fix _id

--- src/actions/DataActions.js
+++ src/actions/DataActions.js
@@ -16,30 +16,30 @@ export function fetchDataAction(data) {
       payload: data,
     })
 
-    setTimeout(()=>{
-      console.log('FETCH_DATA_SUCCESS')
-      dispatch({
-        type: 'FETCH_DATA_SUCCESS',
-        payload: sampleData,
-      });
-      // dispatch(push('/list'));
-    }, 1000);
+    // setTimeout(()=>{
+    //   console.log('FETCH_DATA_SUCCESS')
+    //   dispatch({
+    //     type: 'FETCH_DATA_SUCCESS',
+    //     payload: sampleData,
+    //   });
+    //   // dispatch(push('/list'));
+    // }, 1000);
 
 
-    // axios.post(`${API_PATH}/getZakladInRadius`, {data})
-    //   .then(res => {
-    //     console.log('res', res);
-    //     dispatch({
-    //       type: 'FETCH_DATA_SUCCESS',
-    //       payload: res.data,
-    //     })
-    //   })
-    //   .catch(error => {
-    //     dispatch({
-    //       type: 'FETCH_DATA_FAILURE',
-    //       error,
-    //     })
-    //   })
+    axios.post(`${API_PATH}/getZakladInRadius`, {data})
+      .then(res => {
+        console.log('res', res);
+        dispatch({
+          type: 'FETCH_DATA_SUCCESS',
+          payload: res.data,
+        })
+      })
+      .catch(error => {
+        dispatch({
+          type: 'FETCH_DATA_FAILURE',
+          error,
+        })
+      })
 
   }
 }
